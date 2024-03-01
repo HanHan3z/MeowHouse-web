d3f87408d59cf55641a453cfc8b7b785c7d17669
Natro <127032928+HanHan3z@users.noreply.github.com>
2024-03-01 10:21:26 +0800

    Upload main app

---
+++ Achievement/app.py
@@ -0,0 +1,34 @@
+# AchievementRank
+import json
+from jsonsearch import JsonSearch
+import requests
+
+mergedData = []
+
+for line in open('uidList.txt'):
+    
+    url = 'https://api.mihomo.me/sr_info_parsed/' + str(line)
+    response = requests.get(url)
+    response = json.loads(response.content)
+    
+    uid = str(response['player']['uid'])
+    nickname = str(response['player']['nickname'])
+    level = str(response['player']['level'])
+    signature = str(response['player']['signature'])
+    achievementCount = str(response['player']['space_info']['achievement_count'])
+    
+    data = {
+        'uid': uid,
+        'nickname': nickname,
+        'level': level,
+        'signature': signature,
+        'achievementCount': achievementCount
+    }
+    
+    mergedData.append(data)
+
+mergedData = json.dumps(mergedData, ensure_ascii = False)
+
+file = open('AchievementRank.js', 'w', encoding = 'utf-8')
+file.write(mergedData)
+file.close
