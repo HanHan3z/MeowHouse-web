e872fda8d2951ecab40959e6440e8864a830e159
Natro <127032928+HanHan3z@users.noreply.github.com>
2024-03-03 07:00:42 +0800

    Update app.py

--- Achievement/app.py
+++ Achievement/app.py
@@ -1,5 +1,6 @@
 # AchievementRank
 import json
+import jsonsearch
 import time
 import requests
 
@@ -44,6 +45,23 @@ mergedData = sorted(mergedData, key = lambda j: j['achievementCount'], reverse =
 
 mergedData = json.dumps(mergedData, ensure_ascii = False)
 
-file = open('AchievementRank.json', 'w', encoding = 'utf-8')
-file.write(mergedData)
+jsonData = jsonsearch.JsonSearch(object = mergedData, mode = 'j')
+
+uidArr = jsonData.search_all_value(key = 'uid')
+nicknameArr = jsonData.search_all_value(key = 'nickname')
+levelArr = jsonData.search_all_value(key = 'level')
+signatureArr = jsonData.search_all_value(key = 'signature')
+achievementCountArr = jsonData.search_all_value(key = 'achievementCount')
+
+htmlText = '<table><thead><tr><th class="rank">排名</th><th class="uid">UID</th><th class="nickname">昵称</th><th class="level">开拓等级</th><th class="sign">签名</th><th class="achievementCount">成就数</th></thead><tbody>'
+
+for j in range(0, len(achievementCountArr)):
+    tempRank = j + 1
+    tempText = '<tr><td class="rank">' + str(tempRank) + '</td><td class="uid">'+ str(uidArr[j]) +'</td><td class="nickname">'+ str(nicknameArr[j]) +'</td><td class="level">'+ str(levelArr[j]) +'</td><td class="sign">'+ str(signatureArr[j]) +'</td><td class=""achievementCount>'+ str(achievementCountArr[j]) +'</td></tr>'
+    htmlText = htmlText + tempText
+    
+htmlText = htmlText + '</tbody></table>'
+
+file = open('AchievementRank.html', 'w', encoding = 'utf-8')
+file.write(htmlText)
 file.close()
